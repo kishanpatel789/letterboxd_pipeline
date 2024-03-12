48e44f8fba4af7e55a65ae21e0cf6b605b1be752
Kishan Patel <mail@kishanpatel.dev>
2024-03-12 11:22:56 -0500

    Complete glue dags

--- airflow/dags/02_run_glue_job.py
+++ airflow/dags/02_run_glue_job.py
@@ -10,12 +10,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 from airflow.providers.amazon.aws.operators.glue_crawler import GlueCrawlerOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 
 AIRFLOW_HOME = Path(os.environ.get('AIRFLOW_HOME'))
 AWS_BUCKET = os.environ.get('AWS_BUCKET')
 GLUE_SCRIPT_NAME = 'publish_to_snowflake.py'
+GLUE_CRAWLER_NAME = 'letterboxd_crawler'
+GLUE_JOB_NAME = 'publish_letterboxd_to_snowflake_tf'
 
 logger = logging.getLogger(__name__)
 
@@ -28,21 +30,6 @@ def local_to_s3(file_name, key, bucket_name=AWS_BUCKET):
     s3.load_file(filename=file_name, bucket_name=bucket_name, replace=True, key=key)
     logger.info(f"File '{file_name}' uploaded to 's3://{bucket_name}/{key}'.")
 
-# glue_crawler_config = {
-#     "Name": glue_crawler_name,
-#     "Role": role_arn,
-#     "DatabaseName": glue_db_name,
-#     "Targets": {"S3Targets": [{"Path": f"{bucket_name}/input"}]},
-# }
-
-# def run_glue_crawler():
-#     crawler_operator = AwsGlueCrawlerOperator(
-#         task_id='run_glue_crawler',
-#         crawler_name=GLUE_CRAWLER_NAME,
-#         aws_conn_id='aws_default',
-#         dag=dag,
-#     )
-#     crawler_operator.execute(context=None)
 
 default_args = {
     'owner': 'airflow',
@@ -50,11 +37,6 @@ default_args = {
     'retries': 0,
 }
 
-
-#     crawl_s3 = GlueCrawlerOperator(
-#         task_id="crawl_s3",
-#         config=glue_crawler_config,
-    # )
 
 with DAG(
     dag_id='02_run_glue_job_dag',
@@ -74,4 +56,22 @@ with DAG(
         },
     )
 
-    upload_glue_script_task
+    run_glue_crawler_task = GlueCrawlerOperator(
+        task_id='run_glue_crawler',
+        aws_conn_id='aws_default',
+        wait_for_completion=True,
+        config={
+            "Name": GLUE_CRAWLER_NAME,
+        },
+    )
+
+    run_glue_job_task = GlueJobOperator(
+        task_id='run_glue_job',
+        job_name=GLUE_JOB_NAME,
+        aws_conn_id='aws_default',
+        wait_for_completion=True,
+    )
+
+    upload_glue_script_task >> run_glue_job_task
+    run_glue_crawler_task >> run_glue_job_task
+
